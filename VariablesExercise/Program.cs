52d55a726403906a1da7522406faf522360c85d2
Liam Torres <liam.torreslopez@gmail.com>
2022-10-11 00:23:06 -0700

    wrote story about me

---
+++ VariablesExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string myName = "Liam Torres";
+string friendsName = "Manuel";
+int monthsAgo = 2;
+char nickName = 'L';
+bool parentsPov = true;
+double lastMonth = 12;
+decimal yearsWorking = 2.5m; 
+
+
+Console.WriteLine($"Hello my name is {myName} also known as {nickName}." +
+    $"Currently I’ve being working for Doordash for about {yearsWorking} years now. " +
+    $"Despite my parents mentioning what is {parentsPov} about how I can suffer an accident due to me having my Cerebral Palsy." +
+    " I still am grateful for my job." +
+    $" {monthsAgo} months ago, August to be exact a friend of mine named {friendsName} recommended me to the TrueCoders program so I could explore myself in a new field. " +
+    "Since I started the program I’ve being enjoying it thoroughly that my plan now is to pursue completely this career. " +
+    $"I estimate on finishing the program before month {lastMonth} comes to visit. ");
+
